5e72b414c3285d7b37e0dfd04c72134ce2ba85ef
Johnson Cai <johnson.cai@btcc.com>
2017-11-25 19:47:56 +0800

    Update cmd.js

--- src/lib/cmd.js
+++ src/lib/cmd.js
@@ -3,7 +3,7 @@ import child_process from 'child_process'
 
 export function pbcopy(content) {
   const proc = child_process.spawn('pbcopy')
-  logger.info(`Execute (pbcopy), copy jwt to clipboard, content: ${content}`)
+  logger.info(`Execute (pbcopy), copy content to clipboard, content: ${content}`)
   proc.stdin.write(content)
   proc.stdin.end()
   logger.info(`Now you can execute (CMD+V) to paste your content`)
