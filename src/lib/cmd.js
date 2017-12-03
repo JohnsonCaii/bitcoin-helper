71dd819687503f01a59830364caae5d6a0d5ac09
Johnson Cai <johnson.cai@btcc.com>
2017-12-03 22:12:38 +0800

    style: reformat cmd file style

--- src/lib/cmd.js
+++ src/lib/cmd.js
@@ -2,9 +2,13 @@
 import child_process from 'child_process'
 
 export function pbcopy(content) {
+
   const proc = child_process.spawn('pbcopy')
+
   logger.info(`Execute (pbcopy), copy content to clipboard, content: ${content}`)
+
   proc.stdin.write(content)
   proc.stdin.end()
+
   logger.info(`Now you can execute (CMD+V) to paste your content`)
 }
